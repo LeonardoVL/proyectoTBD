15dd7c1b48db498bdd50d0b3147b42ed2006e3df
manuel-gonzales <141975981+manuel-gonzales@users.noreply.github.com>
2024-08-07 18:03:25 -0500

    se agregó página de gestión de préstamos

---
+++ front/src/components/LoanChart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SubTitle from './SubTitle'
+
+const LoanChart = () => {
+    return (
+        <div className='col-span-5 p-4 bg-[#FFFFF0] rounded-md drop-shadow-md'>
+            <SubTitle subtitle="Índice de préstamos" />
+            Gráfico circular o algo ekis de
+        </div>
+    )
+}
+
+export default LoanChart
