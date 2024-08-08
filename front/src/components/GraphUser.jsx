4c25b0b0e39e3269d663278b2f2a6cfa97107a58
Daniel Sevillano <t022700620@unitru.edu.pe>
2024-08-07 19:43:18 -0500

    graphics

--- front/src/components/GraphUser.jsx
+++ front/src/components/GraphUser.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import SubTitle from './SubTitle'
+import PieChart from './PieChart'
 
 const GraphUser = () => {
   return (
     <div className='col-span-5 p-4 bg-[#FFFFF0] rounded-md drop-shadow-md'>
       <SubTitle subtitle='Gráficos' />
+      <div className='overflow-hidden h-72 w-full flex justify-center items-center'>
+      <PieChart  />
+      </div>
     </div>
   )
 }
